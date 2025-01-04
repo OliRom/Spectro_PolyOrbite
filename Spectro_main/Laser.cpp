38984683638883d9be1621f0dd01c3a167c6d358
Olivier Romain <olivierromain140@gmail.com>
2025-01-04 11:47:41 -0500

    Implémentation des fonctions pour lire la température et la puissance du laser.
    Signed-off-by: Olivier Romain <olivierromain140@gmail.com>

--- Spectro_main/Laser.cpp
+++ Spectro_main/Laser.cpp
@@ -2,7 +2,8 @@
 #include "Laser.h"
 #include "Fonctions.h"
 
-Laser::Laser(byte laser_pin, byte tec_pin, byte pd_pin, byte temp_pin) : _laser_pin(laser_pin), _tec_pin(tec_pin), _pd_pin(pd_pin), _temp_pin(temp_pin) {
+Laser::Laser(byte laser_pin, byte tec_pin, byte pd_pin, byte temp_pin)
+  : _laser_pin(laser_pin), _tec_pin(tec_pin), _pd_pin(pd_pin), _temp_pin(temp_pin) {
   pinMode(_tec_pin, OUTPUT);
   pinMode(_pd_pin, INPUT);
   pinMode(_temp_pin, INPUT);
@@ -13,20 +14,43 @@ Laser::Laser(byte laser_pin, byte tec_pin, byte pd_pin, byte temp_pin) : _laser_
   PWMSetDutyCycle(_laser_pin, _laser_PWM_period * _power / MAX_POWER);
 }
 
-void Laser::activate(bool state){
+void Laser::activate(bool state) {
   PWMStart(_laser_pin, state);
   _state = state;
 }
 
-float Laser::get_power(){
+float Laser::get_power(float V_in, float R) {
+  int meas = analogRead(_temp_pin);        // Lecture de la pin de puissance
+  float V_out = (float)meas * 3.3 / 1024;  // Conversion de la mesure en valeur de tension
 
+  float I = (V_in - V_out) / R;  // Courant circulant dans la photodiode (A)
+  float P = _PD_SLOPE * I;       // Puissance (W)
+
+  return P * 1000;  // Puissance (mW)
 }
 
-float Laser::get_temp(){
+float Laser::get_temp(float V_in, float R) {
+  int meas = analogRead(_temp_pin);        // Lecture de la pin de température
+  float V_out = (float)meas * 3.3 / 1024;  // Conversion de la mesure en valeur de tension
 
+  float R_therm = R * (V_in / V_out - 1);
+  float T_inv = 1 / _T_0 + 1 / _BETA * log(R_therm / _R_0);  // Inverse de la température (K)
+
+  return 1 / T_inv - 273.15;  // Température (oC)
 }
 
-bool Laser::get_state(){ return _state; }
+bool Laser::get_state() {
+  return _state;
+}
 
-void Laser::set_target_power(int power){ _power = power; }
-void Laser::set_target_temp(float temp){ _target_temp = temp; }
+void Laser::set_target_power(int power) {
+  _power = power;
+}
+
+void Laser::set_target_temp(float temp) {
+  _target_temp = temp;
+}
+
+void Laser::set_timeout(int time) {
+  _timeout = time;
+}
--- Spectro_main/Laser.h
+++ Spectro_main/Laser.h
@@ -5,30 +5,40 @@
 
 #define MAX_POWER 500  // Puissance maximale du laser (mW)
 
-class Laser{
-  private:
-    byte _laser_pin;  // Pin d'activation du laser
-    byte _tec_pin;  // Pin du TEC (système de refroidissement)
-    byte _pd_pin;  // Pin de la photodiode (pour la puissance du laser)
-    byte _temp_pin;  // Pin de la thermistance
+// Paramètres de la thermistance
+#define _T_0 298.15  // Tepérature à 25oC (K)
+#define _R_0 10000   // Résistance à 25oC (Ohm)
+#define _BETA 3450   // Paramètre de sensibilité de la thermistance (s.u.)
 
-    bool _state = false;  // État du laser (true: on, false: off)
-    int _laser_PWM_period = 256;  // Période du PWM de la pin d'activation du laser
-    int _power = 5;  // Puissance du laser
+// Paramètre de la photodiode
+#define _PD_SLOPE 250  // P = m * I; m = 250 mW / mA = 250 W / A
 
-    float _target_temp = 25;
+class Laser {
+private:
+  byte _laser_pin;  // Pin d'activation du laser
+  byte _tec_pin;    // Pin du TEC (système de refroidissement)
+  byte _pd_pin;     // Pin de la photodiode (pour la puissance du laser)
+  byte _temp_pin;   // Pin de la thermistance
 
-  public:
-    Laser(byte laser_pin, byte tec_pin, byte pd_pin, byte temp_pin);
+  bool _state = false;          // État du laser (true: on, false: off)
+  int _laser_PWM_period = 256;  // Période du PWM de la pin d'activation du laser
+  int _power = 5;               // Puissance du laser
+  int _timeout = 0;             // Temps après lequel le laser s'éteint (ms)
 
-    void activate(bool state);  // Activer/désactiver le laser
+  float _target_temp = 25;  // Température cible pour le laser (oC)
 
-    float get_power();  // Obtenir la puissance du laser (mW)
-    float get_temp();  // Obtenir la température du laser (oC)
-    bool get_state();  // Obtenir l'état du laser (true: on, false: off)
+public:
+  Laser(byte laser_pin, byte tec_pin, byte pd_pin, byte temp_pin);
 
-    void set_target_power(int power);  // Spécifier la puissance cible du laser (mW)
-    void set_target_temp(float temp);  // Spécifier la température cible du laser (oC)
+  void activate(bool state);  // Activer/désactiver le laser
+
+  float get_power(float V_in, float R);  // Obtenir la puissance du laser (mW)
+  float get_temp(float V_in, float R);   // Obtenir la température du laser (oC); V_in: tension d'alimentation (V), R: résistance de mesure (Ohm)
+  bool get_state();                      // Obtenir l'état du laser (true: on, false: off)
+
+  void set_target_power(int power);  // Spécifier la puissance cible du laser (mW)
+  void set_target_temp(float temp);  // Spécifier la température cible du laser (oC)
+  void set_timeout(int time);        // Spécifier le temps après lequel le laser s'éteint (ms)
 };
 
 #endif
